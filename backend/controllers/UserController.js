2e62bd784a42b758b1838f09987a0848519a4d41
Leandro Felix <leandro123felix123@gmail.com>
2023-04-26 14:45:48 -0300

    Adicionando validações

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,6 +2,49 @@ const User = require("../models/User");
 
 module.exports = class UserController {
   static async register(req, res) {
-    res.json("olá novo usuário!");
+    const { name, email, phone, password, confirmpassword } = req.body;
+
+    //validations
+    if (!name) {
+      res.status(422).json({ message: "O nome é obrigatório!" });
+      return;
+    }
+
+    if (!email) {
+      res.status(422).json({ message: "O email é obrigatório!" });
+      return;
+    }
+
+    if (!phone) {
+      res.status(422).json({ message: "O telefone é obrigatório!" });
+      return;
+    }
+
+    if (!password) {
+      res.status(422).json({ message: "A senha é obrigatória!" });
+      return;
+    }
+
+    if (!confirmpassword) {
+      res
+        .status(422)
+        .json({ message: "A confirmação de senha é obrigatória!" });
+      return;
+    }
+
+    if (password !== confirmpassword) {
+      res.status(422).json({
+        message: "A senha e a confirmação de senha precisam ser iguais!",
+      });
+      return;
+    }
+
+    // check if user exists
+    const userExists = await User.findOne({ email: email });
+
+    if (userExists) {
+      res.status(422).json({ message: "Email já cadastrado!" });
+      return;
+    }
   }
 };
