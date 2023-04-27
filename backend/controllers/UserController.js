4b1b41ea571c6e09107e4d2a3ae7935ac2cef79f
Leandro Felix <leandro123felix123@gmail.com>
2023-04-27 14:07:37 -0300

    Criando função de login no sistema

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -73,4 +73,40 @@ module.exports = class UserController {
       res.status(500).json({ message: error });
     }
   }
+
+  static async login(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      res.status(422).json({ message: "O email é obrigatório!" });
+      return;
+    }
+
+    if (!password) {
+      res.status(422).json({ message: "A senha é obrigatória!" });
+      return;
+    }
+
+    // check if user exists
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+      res.status(422).json({
+        message: "Não há usuário cadastrado com esse email!",
+      });
+      return;
+    }
+
+    //check if password match with db password
+    const checkPassword = await bcrypt.compare(password, user.password);
+
+    if (!checkPassword) {
+      res.status(422).json({
+        message: "Senha incorreta!!",
+      });
+      return;
+    }
+
+    await createUserToken(user, req, res);
+  }
 };
