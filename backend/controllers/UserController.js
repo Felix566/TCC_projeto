0eff43ae75acdf4570edb834012a92aea3275341
Leandro Felix <leandro123felix123@gmail.com>
2023-04-27 15:12:15 -0300

    Implementando método de busca de usuario

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -129,4 +129,19 @@ module.exports = class UserController {
 
     res.status(200).send(currentUser);
   }
+
+  static async getUserById(req, res) {
+    const id = req.params.id;
+
+    const user = await User.findById(id).select("-password");
+
+    if (!user) {
+      res.status(422).json({
+        message: "Usuário não encontrado!",
+      });
+      return;
+    }
+
+    res.status(200).json({ user });
+  }
 };
--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -4,5 +4,7 @@ const UserController = require("../controllers/UserController");
 
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
+router.get("/checkuser", UserController.checkUser);
+router.get("/:id", UserController.getUserById);
 
 module.exports = router;
