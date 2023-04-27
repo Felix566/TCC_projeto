e35d3cf7ab22108e5d6ab29109f2a773cb8deeb4
Leandro Felix <leandro123felix123@gmail.com>
2023-04-27 15:44:34 -0300

    Verificação de token de usuário

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -144,4 +144,11 @@ module.exports = class UserController {
 
     res.status(200).json({ user });
   }
+
+  static async editUser(req, res) {
+    res.status(200).json({
+      message: "deu certo update!",
+    });
+    return;
+  }
 };
