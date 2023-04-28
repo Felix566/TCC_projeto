d97fb1e06b353118222cfab3f5cb9d9b5ef1dafe
Leandro Felix <leandro123felix123@gmail.com>
2023-04-28 10:17:36 -0300

    Estilizando navbar e footer

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -1,6 +1,8 @@
+import styles from "./Footer.module.css";
+
 function Footer() {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <p>
         <span className="bold">Projeto TCC</span> &copy; 2023
       </p>
--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -1,25 +1,29 @@
 import { Link } from "react-router-dom";
 
+import styles from "./Navbar.module.css";
+
 import Logo from "../../assets/img/doacao.png";
 
 function Navbar() {
   return (
-    <nav>
-      <div>
+    <nav className={styles.navbar}>
+      <div className={styles.navbar_logo}>
         <img src={Logo} alt="doação logo" />
         <h2>Projeto TCC</h2>
       </div>
-      <ul>
-        <li>
-          <Link to="/">Usuários</Link>
-        </li>
-        <li>
-          <Link to="/Login">Entrar</Link>
-        </li>
-        <li>
-          <Link to="/register">Cadastrar</Link>
-        </li>
-      </ul>
+      <div className={styles.navbar_menu_links}>
+        <ul className={styles.nav_links}>
+          <li>
+            <Link to="/">Usuários</Link>
+          </li>
+          <li>
+            <Link to="/Login">Entrar</Link>
+          </li>
+          <li>
+            <Link to="/register">Cadastrar</Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
