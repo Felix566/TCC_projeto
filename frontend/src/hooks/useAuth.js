a55133084d6cb83a8d6aba45884ea29c85a5b098
Leandro Felix <leandro123felix123@gmail.com>
2023-04-29 14:04:57 -0300

    Criando hook de autenticação

---
+++ frontend/src/hooks/useAuth.js
@@ -0,0 +1,16 @@
+import api from "../utils/api";
+
+import { useState, UseEffect } from "react";
+import { useHistory } from "react-router-dom";
+
+export default function useAuth() {
+  async function register(user) {
+    try {
+      const data = await api.post("/users/register", user).then((response) => {
+        return response.data;
+      });
+    } catch (error) {}
+  }
+
+  return { register };
+}
