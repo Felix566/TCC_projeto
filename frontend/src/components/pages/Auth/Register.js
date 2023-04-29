fe985e4ad27c17971a289f0dddc6409f836a4c74
Leandro Felix <leandro123felix123@gmail.com>
2023-04-29 12:13:57 -0300

    Criando form de registro

--- frontend/src/components/pages/Auth/Register.js
+++ frontend/src/components/pages/Auth/Register.js
@@ -1,7 +1,49 @@
+import Input from "../../form/Input";
+
 function Register() {
+  function handleChange(e) {}
+
   return (
     <section>
-      <h1>Register</h1>
+      <h1>Registrar</h1>
+      <form>
+        <Input
+          text="Nome"
+          type="text"
+          name="name"
+          placeholder="Digite seu nome"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Telefone"
+          type="text"
+          name="phone"
+          placeholder="Digite seu telefone"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="E-mail"
+          type="email"
+          name="email"
+          placeholder="Digite seu E-mail"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Senha"
+          type="password"
+          name="password"
+          placeholder="Digite sua senha"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Confirmação de Senha"
+          type="password"
+          name="confirmpassword"
+          placeholder="Confirme a sua senha"
+          handleOnChange={handleChange}
+        />
+        <input type="submit" value="Cadastrar" />
+      </form>
     </section>
   );
 }
