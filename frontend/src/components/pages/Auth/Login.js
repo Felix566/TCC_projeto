770467b1f5890c40f936183e913fc99562d703b2
Leandro Felix <leandro123felix123@gmail.com>
2023-04-29 18:30:28 -0300

    Criando formulário de login

--- frontend/src/components/pages/Auth/Login.js
+++ frontend/src/components/pages/Auth/Login.js
@@ -1,7 +1,38 @@
+import { useState, useContext } from "react";
+import Input from "../../form/Input";
+import { Link } from "react-router-dom";
+
+import styles from "../../form/Form.module.css";
+
+/* context */
+import { Context } from "../../../context/UserContext";
+
 function Login() {
+  function handleChange(e) {}
+
   return (
-    <section>
+    <section className={styles.form_container}>
       <h1>Login</h1>
+      <form>
+        <Input
+          text="Email"
+          type="email"
+          name="email"
+          placeholder="Digite seu e-mail"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Senha"
+          type="password"
+          name="password"
+          placeholder="Digite sua senha"
+          handleOnChange={handleChange}
+        />
+        <input type="submit" value="Entrar" />
+      </form>
+      <p>
+        Não é registrado? <Link to="/register">Clique aqui!</Link>
+      </p>
     </section>
   );
 }
