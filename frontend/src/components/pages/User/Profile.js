0125fb793d62602ed379d261be28af3d3b915081
Leandro Felix <leandro123felix123@gmail.com>
2023-05-02 09:32:30 -0300

    adicção de dependencia

--- frontend/src/components/pages/User/Profile.js
+++ frontend/src/components/pages/User/Profile.js
@@ -1,7 +1,105 @@
+import api from "../../../utils/api";
+
+import { useState, useEffect } from "react";
+
+import styles from "./Profile.module.css";
+import formStyles from "../../form/Form.module.css";
+
+import Input from "../../form/Input";
+
+import useflashMessage from "../../../hooks/useFlashMessage";
+
 function Profile() {
+  const [user, setUser] = useState({});
+  const [token] = useState(localStorage.getItem("token") || "");
+
+  useEffect(() => {
+    api
+      .get("/users/checkuser", {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(token)}`,
+        },
+      })
+      .then((response) => {
+        setUser(response.data);
+      });
+  }, [token]);
+
+  function handleChange(e) {
+    setUser({ ...user, [e.target.name]: e.targer.value });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    let msgText = "Dados atualizados com sucesso!";
+    let msgType = "sucess";
+
+    try {
+      const data = await api
+        .put(`/users/edit/${user._id}`, user, {})
+        .then((response) => {
+          return response.data;
+        });
+    } catch (error) {
+      msgText = error.response.data.message;
+      msgType = "error";
+    }
+
+    setFlashMessage(msgText, msgType);
+  }
+
   return (
     <section>
-      <h1>Profile</h1>
+      <div className={styles.profile_header}>
+        <h1>Profile</h1>
+        <p>Edite suas Informações</p>
+      </div>
+      <form onSubmit={handleSubmit} className={formStyles.form_container}>
+        <Input
+          text="E-mail"
+          type="email"
+          name="email"
+          placeholder="Digite seu email"
+          handleOnChange={handleChange}
+          value={user.email || ""}
+        />
+
+        <Input
+          text="Nome"
+          type="text"
+          name="name"
+          placeholder="Digite seu nome"
+          handleOnChange={handleChange}
+          value={user.name || ""}
+        />
+
+        <Input
+          text="Telefone"
+          type="text"
+          name="phone"
+          placeholder="Digite seu telefone"
+          handleOnChange={handleChange}
+          value={user.phone || ""}
+        />
+
+        <Input
+          text="Senha"
+          type="password"
+          name="password"
+          placeholder="Digite sua senha"
+          handleOnChange={handleChange}
+        />
+
+        <Input
+          text="Confirmação de Senha"
+          type="password"
+          name="confirmpassword"
+          placeholder="Confirme sua senha"
+          handleOnChange={handleChange}
+        />
+        <input type="submit" value="Editar" />
+      </form>
     </section>
   );
 }
